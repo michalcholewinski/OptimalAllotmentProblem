111500eff8e954dc5f4db39a9434abb2995cbbc3
Michal Cholewinski <cholewinskimichal@gmail.com>
2014-11-04 23:39:49 +0100

    Deleting irrelevant file

--- create_child_projects.bat
+++
@@ -1,7 +0,0 @@
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_Client
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_DB
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_DB_Api
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_DAO
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_DAO_Api
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_Facade
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=cholewinski -DartifactId=OAP_SWD_Facade_Api
